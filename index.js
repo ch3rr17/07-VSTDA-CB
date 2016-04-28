84bac7a89f022dbde06839a9f41913f58f260736
ch3rr17 <cherr.batac@gmail.com>
2016-04-28 15:56:41 -0700

    1st commit - pending filters

---
+++ index.js
@@ -0,0 +1,30 @@
+var app = angular.module('myApp', []); 
+app.controller('MainController', ['$scope', function($scope){
+    
+    $scope.todoLists = [];
+    
+    $scope.todoAdd = function() {
+        $scope.todoLists.push({
+            'list': $scope.addItem,
+            'done': false,
+            'priority': $scope.itemColor
+        });
+        $scope.addItem = ''
+    };
+
+    $scope.predicate = 'list';
+    $scope.reverse = true;
+    
+    $scope.order = function(predicate) {
+
+        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+        $scope.predicate = predicate;
+    };
+
+    $scope.delete = function(index) {
+            $scope.todoLists.splice(index,1);
+        };
+
+
+
+}]);
